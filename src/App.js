2bc1644935f41393435e65d00915912fa8398e5a
Hardik <hardik12c@gmail.com>
2022-08-05 17:38:13 +0530

    i have added props in my card section for practicing props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import card1 from './images/card1.png';
+import card2 from './images/card2.png';
+import card3 from './images/card3.png';
 import Navbar from './components/navbar';
 import Section1 from './components/section1';
 import Cards from './components/cards';
@@ -7,7 +10,29 @@ function App() {
     <>
       <Navbar/>
       <Section1/>
-      <Cards/>
+      <section className="cardsection">
+        <Cards 
+        cardimg={card2}
+        title="Life Lessons with Katie Zaferes" 
+        price="$136" 
+        rating={5.0} 
+        review={6} 
+        country="USA" />
+        <Cards 
+        cardimg={card3}
+        title="Life Lessons with Katie Zaferes" 
+        price="$136" 
+        rating={5.0} 
+        review={6} 
+        country="USA" />
+        <Cards 
+        cardimg={card1}
+        title="Life Lessons with Katie Zaferes" 
+        price="$136" 
+        rating={5.0} 
+        review={6} 
+        country="USA" />
+      </section>
     </>
   );
 }
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,38 +1,15 @@
 import React from 'react'
-import card1 from '../images/card1.png'
-import card2 from '../images/card2.png'
-import card3 from '../images/card3.png'
 import star from '../images/Star.png'
-export default function Cards() {
-  return (
-    <div className='cardsection'>
+export default function Cards(props) {
+    return (
         <div className="cardbox">
-            <img src={card2} alt="" />
+            <img src={props.cardimg} alt="error" />
             <div className="cardinfo">
                 <img src={star} alt="error" />
-                <span>5.0 (6) •USA</span>
+                <span>{props.rating} ({props.review}) •{props.country}</span>
             </div>
-            <span>Life lessons with Katie Zaferes</span>
-            <span>From $136 / person</span>
+            <span>{props.title}</span>
+            <span><span className='bold'>From {props.price}</span> / person</span>
         </div>
-        <div className="cardbox">
-            <img src={card3} alt="" />
-            <div className="cardinfo">
-                <img src={star} alt="error" />
-                <span>5.0 (30) •USA</span>
-            </div>
-            <span>Learn wedding photography</span>
-            <span>From $125 / person</span>
-        </div>
-        <div className="cardbox">
-            <img src={card1} alt="" />
-            <div className="cardinfo">
-                <img src={star} alt="error" />
-                <span>4.8 (2) •USA</span>
-            </div>
-            <span>Group Mountain Biking</span>
-            <span>From $50 / person</span>
-        </div>
-    </div>
-  )
+    )
 }
